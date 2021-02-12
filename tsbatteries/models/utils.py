382fb676c66ab35fdede032efb0b5fed51c9669d
James Morrill <james.morrill.6@gmail.com>
2021-02-12 14:09:28 +0000

    Merge develop and update to v0.1.2

--- tsbatteries/models/utils.py
+++ tsbatteries/models/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import torch
+from torch import nn
 
 
 class StaticTemporalTensorDataset:
@@ -16,6 +18,29 @@ class StaticTemporalTensorDataset:
 
     def __len__(self):
         return len(self.labels)
+
+
+class TimeSeriesLossWrapper(nn.Module):
+    """Applies a given loss function along the length of the outputs removing any nans in the given labels.
+
+    Given a PyTorch loss function and target labels of shape [N, L, C] (as opposed to [N, C]), this removes any
+    values where labels are nan (corresponding to a finished time series) and computes the loss against the labels
+    and predictions in the remaining non nan locations.
+    """
+
+    def __init__(self, criterion):
+        """
+        Args:
+            criterion (nn.Module): A pytorch loss function.
+        """
+        super().__init__()
+
+        assert isinstance(criterion, nn.Module)
+        self.criterion = criterion
+
+    def forward(self, preds, labels):
+        mask = ~torch.isnan(labels)
+        return self.criterion(preds[mask], labels[mask])
 
 
 def get_num_params(model):
@@ -47,7 +72,11 @@ def tune_number_of_parameters(model_builder, desired_num_params):
     """
     min_params = get_num_params(model_builder(1))
     if min_params > desired_num_params:
-        raise ValueError("Minimum number of model params ({}) is more than desired ({})".format(min_params, desired_num_params))
+        raise ValueError(
+            "Minimum number of model params ({}) is more than desired ({})".format(
+                min_params, desired_num_params
+            )
+        )
 
     num_params = 0
     hidden_dim = 1
@@ -59,7 +88,10 @@ def tune_number_of_parameters(model_builder, desired_num_params):
     # Revert back one if it is closer
     if hidden_dim - 2 > 0:
         prev_build = model_builder(hidden_dim - 2)
-        if desired_num_params - get_num_params(prev_build) < desired_num_params - num_params:
+        if (
+            desired_num_params - get_num_params(prev_build)
+            < desired_num_params - num_params
+        ):
             model = prev_build
 
     return model
